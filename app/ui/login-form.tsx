01e547dda5e5f92791d77877e19f702be755f94c
Luis Rivera <rluis747@gmail.com>
2025-03-06 10:00:16 -0500

    fix bug

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -15,7 +15,7 @@ export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
+    (state: any, formData: FormData) => authenticate(formData),
     undefined,
   );
   return (
